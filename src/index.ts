a8bc07f0d0f5d88dd4c7c5e2692b0376e1c0273c
Wizard <cristiandamianvena@gmail.com>
2024-09-09 19:32:45 -0300

    fix: added funnel stuff

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import {NextFunction, Request, Response} from 'express';
 import express from 'express';
 import logger from './utils/logger';
 import dotenv from 'dotenv';
+import * as crypto from 'crypto';
 
 dotenv.config();
 import client from 'prom-client';
+import {faker} from "@faker-js/faker";
 
 const app = express();
 
@@ -27,6 +29,35 @@ const responseTimeHistogram = new client.Histogram({
 });
 
 register.registerMetric(responseTimeHistogram)
+
+const stockValidation = new client.Counter({
+    name: 'stock_validation',
+    help: 'Stock validation on Warehouse system',
+    labelNames: [],
+})
+
+const paymentValidationSuccess = new client.Counter({
+    name: 'payment_validation_success',
+    help: 'Payment execution success',
+    labelNames: [],
+});
+
+const paymentValidationFailed = new client.Counter({
+    name: 'payment_validation_failed',
+    help: 'Payment execution failed',
+    labelNames: [],
+})
+
+const checkoutProcessStarted = new client.Counter({
+    name: 'checkout_process_started',
+    help: 'Checkout process started',
+    labelNames: [],
+})
+
+register.registerMetric(checkoutProcessStarted)
+register.registerMetric(stockValidation)
+register.registerMetric(paymentValidationSuccess)
+register.registerMetric(paymentValidationFailed)
 
 type MeasurableRequest = Request & { startTime: number };
 
@@ -51,8 +82,57 @@ app.use((req: MeasurableRequest, res: Response, next: NextFunction) => {
     next();
 });
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('OK');
+const wrapResolve = (probability: number) => {
+    return new Promise((resolve, reject) => {
+        const result = faker.helpers.maybe(
+            () => {
+                return 'ok'
+            },
+            {probability}
+        )
+        if (result) {
+            resolve({})
+        } else {
+            reject()
+        }
+    })
+}
+
+const validateStock = async (body: any) => {
+    const promise = wrapResolve(0.95);
+
+    await promise
+    stockValidation.inc()
+}
+
+const validatePayment = async (body: any) => {
+    const promise = wrapResolve(0.8)
+
+    try {
+        await promise;
+        paymentValidationSuccess.inc()
+    } catch (e) {
+        paymentValidationFailed.inc()
+        throw e;
+    }
+}
+
+app.post('/payments', async (req: Request, res: Response) => {
+
+    try {
+        const body = req.body;
+
+        checkoutProcessStarted.inc()
+
+        await validateStock(body);
+
+        await validatePayment(body)
+
+        res.send('OK');
+    } catch (e) {
+        res.status(400).json({message: 'failed process'})
+    }
+
 });
 
 app.get('/metrics', async (req, res) => {
